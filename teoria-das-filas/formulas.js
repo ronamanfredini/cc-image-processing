67b7edd0c5c9f1bee4ad0848ffc94a35a50dbe1e
João Paulo Malvesti <jpmf1995@gmail.com>
2022-03-21 20:26:15 -0300

    Formulas

---
+++ teoria-das-filas/formulas.js
@@ -0,0 +1,19 @@
+class Formulas {
+	
+	static taxaMediaDeUtilizacao(chegada, servico) {
+		return chegada / servico;
+	}
+
+	static numeroMedioDeClientesSistema(chegada, servico) {
+		return chegada / (servico - chegada);
+	}
+
+	static tempoMedioDespendidoNoSistema(chegada, servico) {
+		return 1 / (servico - chegada)
+	}
+
+	static probabilidadeNClientesNoLocal(chegada, servico, n) {
+		return (1 - (chegada / servico)) * (chegada / servico)**n;
+	}
+}
+
