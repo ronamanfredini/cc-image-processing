dc52fad5c2de4734dd62e203380425235f203ffa
Ronaldo Manfredini <ronaldo@itcraftship.com>
2022-03-21 20:05:01 -0300

    mais coisa

---
+++ teoria-das-filas/script.js
@@ -0,0 +1,19 @@
+function run() {
+  const clientMaxProbability = document.getElementById('client-max-prob').value;
+  buildResultTable(clientMaxProbability);
+}
+
+function buildResultTable() {
+  const resultTable = document.getElementById('result-table');
+  resultTable.innerHTML = null;
+}
+
+function buildRow() {
+
+}
+
+function buildCell() {
+
+}
+
+
