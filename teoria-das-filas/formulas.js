0cfd5ffa47de9cf0f0182285905b45b3ebb036bc
João Paulo Malvesti <jpmf1995@gmail.com>
2022-03-22 01:09:59 -0300

    Added method toFixed to return 2 decimal float

--- teoria-das-filas/formulas.js
+++ teoria-das-filas/formulas.js
@@ -1,23 +1,23 @@
 class Formulas {
 
 	static converteParaHoras(valor) {
-		return 60 / valor;
+		return (60 / valor).toFixed(2);
 	}
 
 	static taxaMediaDeUtilizacao(chegada, servico) {
-		return chegada / servico;
+		return (chegada / servico).toFixed(2);
 	}
 
 	static numeroMedioDeClientesSistema(chegada, servico) {
-		return chegada / (servico - chegada);
+		return (chegada / (servico - chegada)).toFixed(2);
 	}
 
 	static tempoMedioDespendidoNoSistema(chegada, servico) {
-		return 1 / (servico - chegada)
+		return (1 / (servico - chegada)).toFixed(2);
 	}
 
 	static probabilidadeNClientesNoLocal(chegada, servico, n) {
-		return (1 - (chegada / servico)) * (chegada / servico)**n;
+		return ((1 - (chegada / servico)) * (chegada / servico)**n).toFixed(2);
 	}
 }
 
