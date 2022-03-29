6671237fcbc11b075284f7128ca8340cfd18c7a0
Joni Milczarek <jonatanmilc@gmai.com>
2022-03-28 23:09:51 -0300

    finished table simulation

--- tabela-simulacao/script.js
+++ tabela-simulacao/script.js
@@ -1,12 +1,53 @@
 function run() {
-  const tecs = document.getElementById('tec').value.split('\n');
-  const ts = document.getElementById('ts').value.split('\n');
-  const tempo = document.getElementById('tempo').value;
-  console.log(tecs, ts, tempo)
+  const tecs = document.getElementById('tec').value.split('\n').map(item => parseFloat(item));
+  const ts = document.getElementById('ts').value.split('\n').map(item => parseFloat(item));
+  const tempo = parseFloat(document.getElementById('tempo').value);
+  var returnArray = SimulaTabela.runSimulation(tempo, tecs, ts);
+  buildResultTable(returnArray);
 }
 
 function clear() {
   document.getElementById('tec').value = null;
   document.getElementById('ts').value = null;
   document.getElementById('tempo').value = null;
+}
+
+function buildResultTable(returnArray) {
+  tableArray = returnArray[0];
+  const resultTable = document.getElementById('result-table');
+  for (let i = 0; i < tableArray.length; i++) {
+    const row = buildRow(tableArray[i]);
+    resultTable.append(row);
+  }
+  resultTable.append(buildRowTotals(returnArray[1]));
+}
+
+function buildRow(tableRow) {
+   let row = document.createElement('tr');
+   Object.values(tableRow).map(item => {
+     row.append(buildCell(item));
+   })
+
+   return row;
+}
+
+function buildRowTotals(rowTotals) {
+  let row = document.createElement('tr');
+  console.log(rowTotals);
+  row.append(buildCell(''));
+  row.append(buildCell(''));
+  row.append(buildCell(''));
+  row.append(buildCell(rowTotals.tempoServico));
+  row.append(buildCell(''));
+  row.append(buildCell(rowTotals.tempoClienteFila));
+  row.append(buildCell(''));
+  row.append(buildCell(rowTotals.tempoClienteSistema));
+  row.append(buildCell(rowTotals.tempoLivreOperador));
+  return row;
+}
+
+function buildCell(value) {
+  let cell = document.createElement('td');
+  cell.append(value);
+  return cell;
 }
--- tabela-simulacao/simulaTabela.js
+++ tabela-simulacao/simulaTabela.js
@@ -1,11 +1,16 @@
 class SimulaTabela {
-
 	static runSimulation(tempo, tec, ts) {
 		console.log(tempo);
 		console.log(tec);
 		console.log(ts);
 		let tableArray = [];
 		let clientId = 1;
+		let totals = {
+			tempoServico: 0,
+			tempoClienteFila: 0,
+			tempoClienteSistema: 0,
+			tempoLivreOperador: 0
+		}
 		for (let i = 0; i <= tempo; i++) {
 			let table = {
 				cliente: 0,
@@ -15,7 +20,7 @@ class SimulaTabela {
 				tempoInicioServico: 0,
 				tempoClienteFila: 0,
 				tempoFinalServico: 0,
-				tempoServicoFila: 0,
+				tempoDoCliente: 0,
 				tempoLivreDoOperador: 0
 			};
 
@@ -24,25 +29,38 @@ class SimulaTabela {
 			table.tempoServicoRandom = ts[this.randomArray(ts)];
 			if (tableArray.length < 1) {
 				table.tempoChegada = table.tempoChegadaRandom;
-				table.tempoInicioServico = table.tempoServicoRandom;
+				table.tempoInicioServico = table.tempoChegadaRandom;
 				table.tempoLivreDoOperador = table.tempoChegadaRandom;
+				table.tempoClienteFila = 0;
+				totals.tempoLivreOperador = table.tempoChegadaRandom;
+				totals.tempoServico = table.tempoServicoRandom;
 			} else {
 				table.tempoChegada = tableArray[clientId - 2].tempoChegada + table.tempoChegadaRandom;
-				table.tempoInicioServico = tableArray[clientId - 2].tempoInicioServico + table.tempoChegadaRandom + table.tempoServicoFila;
-				table.tempoLivreDoOperador = tableArray[clientId - 2].tempoFinalServico + table.tempoChegada;
+				table.tempoInicioServico = tableArray[clientId - 2].tempoInicioServico + table.tempoChegadaRandom + table.tempoDoCliente;
+				table.tempoLivreDoOperador = tableArray[clientId - 2].tempoFinalServico < table.tempoChegada ? table.tempoChegada - tableArray[clientId - 2].tempoFinalServico : 0;
+				totals.tempoLivreOperador = totals.tempoLivreOperador + table.tempoLivreDoOperador;
+				totals.tempoServico = totals.tempoServico + table.tempoServicoRandom;
+				if (tableArray[clientId - 2].tempoFinalServico >= table.tempoChegada) {
+					table.tempoClienteFila = tableArray[clientId - 2].tempoFinalServico - table.tempoChegada;
+					totals.tempoClienteFila = totals.tempoClienteFila + table.tempoClienteFila;
+				} else {
+					table.tempoClienteFila = 0;
+				}
 			}
-
+			table.tempoFinalServico = table.tempoInicioServico + table.tempoServicoRandom;
+			table.tempoDoCliente = table.tempoServicoRandom + table.tempoClienteFila;
+			totals.tempoClienteSistema = totals.tempoClienteSistema + table.tempoDoCliente;
 			tableArray.push(table);
-			console.log(tableArray);
-			
 			clientId++;
 			if (tempo < table.tempoInicioServico) break;
 		}
-
+		return [tableArray, totals];
 	}
 
 	static randomArray(array) {
 		return (Math.random() * (array.length - 1)).toFixed(0);
 	}
+
+	
 }
 
