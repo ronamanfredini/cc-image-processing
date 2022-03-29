6fd32835efd2e59fff70b03588c6debf0abc2952
João Paulo Malvesti <jpmf1995@gmail.com>
2022-03-29 01:43:07 -0300

    create table final result

--- tabela-simulacao/script.js
+++ tabela-simulacao/script.js
@@ -4,12 +4,18 @@ function run() {
   const tempo = parseFloat(document.getElementById('tempo').value);
   var returnArray = SimulaTabela.runSimulation(tempo, tecs, ts);
   buildResultTable(returnArray);
+  buildFinalResult(returnArray);
 }
 
-function clear() {
+function clearPage() {
+  console.log('clear');
+
   document.getElementById('tec').value = null;
   document.getElementById('ts').value = null;
   document.getElementById('tempo').value = null;
+
+  clearTable();
+  clearFinalTable();
 }
 
 function buildResultTable(returnArray) {
@@ -50,4 +56,53 @@ function buildCell(value) {
   let cell = document.createElement('td');
   cell.append(value);
   return cell;
+}
+
+function clearTable(){
+  const resultTable = document.getElementById('result-table');
+  resultTable.innerHTML = '';
+}
+
+function buildFinalResult(returnArray){
+  const totalClients = returnArray[0].length;
+  const totalTimeCustomersOnqueue = returnArray[1].tempoClienteFila;
+  const totalFreeTimeOperator = returnArray[1].tempoLivreOperador;
+  const serviceFinalTime = returnArray[0][totalClients - 1].tempoFinalServico;
+  const totalTimeService = returnArray[1].tempoServico;
+  const totalTimeCustomersOnSistem = returnArray[1].tempoClienteSistema;
+  
+  let waitTimeOnQueueRow = document.getElementById("wait-time-on-queue");
+  let probWaitClientOnQueueRow = document.getElementById("probability-wait-client-on-queue");
+  let probFreeOperatorRow = document.getElementById("probability-free-operator");
+  let mediumTimeServiceRow = document.getElementById("medium-time-service");
+  let mediumTimeOutSistemRow = document.getElementById("medium-time-out-sistem");
+  
+  let waitTimeOnQueue = (totalTimeCustomersOnqueue / totalClients).toFixed(2);
+  waitTimeOnQueueRow.append(buildCell(`${waitTimeOnQueue} minutos`));
+
+  let probWaitClientOnQueue = (totalTimeCustomersOnqueue / totalClients * 100).toFixed(2);
+  probWaitClientOnQueueRow.append(buildCell(`${probWaitClientOnQueue} %`));
+
+  let probFreeOperator = (totalFreeTimeOperator / serviceFinalTime * 100).toFixed(2);
+  probFreeOperatorRow.append(buildCell(`${probFreeOperator} %`));
+
+  let mediumTimeService = (totalTimeService / totalClients).toFixed(2);
+  mediumTimeServiceRow.append(buildCell(`${mediumTimeService} minutos`));
+
+  let mediumTimeOutSistem = (totalTimeCustomersOnSistem / totalClients).toFixed(2);
+  mediumTimeOutSistemRow.append(buildCell(`${mediumTimeOutSistem} minutos`));
+}
+
+function clearFinalTable(){
+  let waitTimeOnQueueRow = document.getElementById("wait-time-on-queue");
+  let probWaitClientOnQueueRow = document.getElementById("probability-wait-client-on-queue");
+  let probFreeOperatorRow = document.getElementById("probability-free-operator");
+  let mediumTimeServiceRow = document.getElementById("medium-time-service");
+  let mediumTimeOutSistemRow = document.getElementById("medium-time-out-sistem");
+  
+  waitTimeOnQueueRow.removeChild(waitTimeOnQueueRow.getElementsByTagName('td')[0]);
+  probWaitClientOnQueueRow.removeChild(probWaitClientOnQueueRow.getElementsByTagName('td')[0]);
+  probFreeOperatorRow.removeChild(probFreeOperatorRow.getElementsByTagName('td')[0]);
+  mediumTimeServiceRow.removeChild(mediumTimeServiceRow.getElementsByTagName('td')[0]);
+  mediumTimeOutSistemRow.removeChild(mediumTimeOutSistemRow.getElementsByTagName('td')[0]);
 }
