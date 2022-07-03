40b629a26811114148f8cb376d0e316afe486907
Ronaldo Manfredini <ronaldo@nursa.com>
2022-07-03 13:52:04 -0300

    .

--- histograma/minimum-filter.js
+++ histograma/minimum-filter.js
@@ -6,7 +6,7 @@ function minimumFilter(matrix, width, filterSize) {
 
   for (let i = 0; i< lineAndChunkSeparated.length; i++) {
     for (let j = 0; j < lineAndChunkSeparated[i].length; j++) {
-      let minimumValue = [256, 256, 256, 256];
+      let minimumValue = [255, 255, 255, 255];
 
       for (let l = i - kernelSize; l < i + kernelSize; l++) {
         for (let m = j - kernelSize; m <= j + kernelSize; m++) {
@@ -22,8 +22,7 @@ function minimumFilter(matrix, width, filterSize) {
 
       lineAndChunkSeparated[i][j][0] = minimumValue[0];
       lineAndChunkSeparated[i][j][1] = minimumValue[1];
-      //TODO ajustar Blue
-      lineAndChunkSeparated[i][j][2] = minimumValue[1];
+      lineAndChunkSeparated[i][j][2] = minimumValue[2];
       lineAndChunkSeparated[i][j][3] = 255;
     }
   }
