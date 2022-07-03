c2ad0686ccaea56d9ac547a479a1864554f37a0a
Ronaldo Manfredini <ronaldo@nursa.com>
2022-07-03 15:03:41 -0300

    feat: average filter working

--- histograma/average-filter.js
+++ histograma/average-filter.js
@@ -7,29 +7,24 @@ const kernelMapping = {
 function averageFilter(matrix, width, filterSize = 3) {
   const lineSeparated = separateMatrixIntoLines(matrix, width);
   const lineAndChunkSeparated = separateChunkIntoPixels(lineSeparated);
-  const kernelSize = kernelMapping[filterSize.toString()];
-  // const averageBias = 1 / (filterSize * filterSize);
+  const kernelSize = (filterSize - 1) / 2;
+  const totalPixelCount = filterSize * filterSize;
 
-  for (let i = 0; i< lineAndChunkSeparated.length; i++) {
-    for (let j = 0; j < lineAndChunkSeparated[i].length; j++) {
-      let totalPixelCount = 0;
+  for (let i = kernelSize; i < lineAndChunkSeparated.length - kernelSize; i++) {
+    for (let j = kernelSize; j < lineAndChunkSeparated[i].length - kernelSize; j++) {
       let totalPixelSum = [0, 0, 0, 0];
 
-      for (let l = i - kernelSize; l < i + kernelSize; l++) {
+      for (let l = i - kernelSize; l <= i + kernelSize; l++) {
         for (let m = j - kernelSize; m <= j + kernelSize; m++) {
-          if (lineAndChunkSeparated[l] && lineAndChunkSeparated[l][m]) {
-            totalPixelCount++;
-            totalPixelSum[0] += lineAndChunkSeparated[l][m][0];
-            totalPixelSum[1] += lineAndChunkSeparated[l][m][1];
-            totalPixelSum[2] += lineAndChunkSeparated[l][m][2];
-          }
+          totalPixelSum[0] += lineAndChunkSeparated[l][m][0];
+          totalPixelSum[1] += lineAndChunkSeparated[l][m][1];
+          totalPixelSum[2] += lineAndChunkSeparated[l][m][2];
         }
       }
 
       lineAndChunkSeparated[i][j][0] = totalPixelSum[0] / totalPixelCount;
       lineAndChunkSeparated[i][j][1] = totalPixelSum[1] / totalPixelCount;
-      // TODO ajustar Blue
-      lineAndChunkSeparated[i][j][2] = lineAndChunkSeparated[i][j][1];
+      lineAndChunkSeparated[i][j][2] = totalPixelSum[2] / totalPixelCount;
       lineAndChunkSeparated[i][j][3] = 255;
     }
   }
