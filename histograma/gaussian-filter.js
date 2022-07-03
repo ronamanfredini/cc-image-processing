cb395f9a05ac7e3e65668f20aef8bfa6bd54f602
Ronaldo Manfredini <ronaldo@nursa.com>
2022-07-03 16:17:05 -0300

    feat: added filter size

--- histograma/gaussian-filter.js
+++ histograma/gaussian-filter.js
@@ -3,26 +3,26 @@ function gaussianFilter(matrix, width, filterSize) {
   const lineAndChunkSeparated = separateChunkIntoPixels(lineSeparated);
   const img2 = JSON.parse(JSON.stringify(lineAndChunkSeparated));
   const kernelSize = (filterSize - 1) / 2;
+  const bias = 4;
 
   for (let i = kernelSize; i < lineAndChunkSeparated.length - kernelSize; i++) {
     for (let j = kernelSize; j < lineAndChunkSeparated[i].length - kernelSize; j++) {
-      let totalPixelSum = [[], [], [], []];
+      let totalPixelSum = [0,0,0,0];
+      let div = 0;
 
       for (let l = i - kernelSize; l <= i + kernelSize; l++) {
         for (let m = j - kernelSize; m <= j + kernelSize; m++) {
-          if (l == i && m == j) {
-            continue;
-          } else {
-            totalPixelSum[0].push(lineAndChunkSeparated[l][m][0]);
-            totalPixelSum[1].push(lineAndChunkSeparated[l][m][1]);
-            totalPixelSum[2].push(lineAndChunkSeparated[l][m][2]);
-          }
+          const val = (1 / (2 * Math.PI * (bias * bias))) * Math.exp(-1 * ((l * l) * (m * m)) / (2 * bias * bias));
+          totalPixelSum[0] += lineAndChunkSeparated[l][m][0] * val;
+          totalPixelSum[1] += lineAndChunkSeparated[l][m][1] * val;
+          totalPixelSum[2] += lineAndChunkSeparated[l][m][2] * val;
+          div += val;
         }
       }
 
-      img2[i][j][0] = Math.max(...totalPixelSum[0]);
-      img2[i][j][1] = Math.max(...totalPixelSum[1]);
-      img2[i][j][2] = Math.max(...totalPixelSum[2]);
+      img2[i][j][0] = Math.floor(totalPixelSum[0] / div);
+      img2[i][j][1] = Math.floor(totalPixelSum[1] / div);
+      img2[i][j][2] = Math.floor(totalPixelSum[2] / div);
       img2[i][j][3] = 255;
     }
   }
